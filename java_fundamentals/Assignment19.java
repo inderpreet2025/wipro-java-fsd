3e6ace4ec42544df650e32a4b3e3417cdb982d05
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 17:03:28 +0000

    -m

---
+++ java_fundamentals/Assignment19.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Assignment19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int reversedNumber = reverseNumber(number);
+
+        System.out.println("Reversed number: " + reversedNumber);
+
+        sc.close();
+    }
+
+    public static int reverseNumber(int number) {
+        int reversed = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+
+        return reversed;
+    }
+}
