d4c795808cfa0260c275646e0505d524d59a7fe0
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 16:22:24 +0000

    -m

---
+++ java_fundamentals/Assignment3.java
@@ -0,0 +1,16 @@
+public class Assignment3 {
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            try {
+                int num1 = Integer.parseInt(args[0]);
+                int num2 = Integer.parseInt(args[1]);
+                int sum = num1 + num2;
+                System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Both arguments must be integers.");
+            }
+        } else {
+            System.out.println("Error: Exactly two integer arguments are required.");
+        }
+    }
+}
