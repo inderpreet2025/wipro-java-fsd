1e96fa1ad7e7a3d570601fa4f237dad39cc59dd3
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 16:40:06 +0000

    -m

---
+++ java_fundamentals/Assignment5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
