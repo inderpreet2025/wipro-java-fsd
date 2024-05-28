e9145fe05e9041394e5d621fe2ff54603b92d681
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 16:32:00 +0000

    -m

---
+++ java_fundamentals/Assignment4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assignment4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println(number + " is zero.");
+        }
+    }
+}
