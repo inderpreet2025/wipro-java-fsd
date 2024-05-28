6f5faf085c2b77dc1fb12131c5f45ac3fe06c367
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 17:11:44 +0000

    -m

---
+++ java_fundamentals/Assignment15.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Assignment15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPrime(int number) {
+        // Prime numbers are greater than 1
+        if (number <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
