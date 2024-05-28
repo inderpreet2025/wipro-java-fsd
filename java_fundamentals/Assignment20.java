f28fc7f058ba77a5e1053dae5d76b827a09f395b
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-28 16:59:27 +0000

    -m

---
+++ java_fundamentals/Assignment20.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Assignment20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
