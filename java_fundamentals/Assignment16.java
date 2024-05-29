d1907ae822c4c247146dfd5ed8893caf8707f92e
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-29 11:08:21 +0000

    -m

---
+++ java_fundamentals/Assignment16.java
@@ -0,0 +1,22 @@
+public class Assignment16 {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 10 and 99 are:");
+        for (int i = 10; i <= 99; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true;
+    }
+}
