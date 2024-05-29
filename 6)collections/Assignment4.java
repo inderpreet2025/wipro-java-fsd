3f7d0d1bbaf2986d7ff7faf65b0914567e61e610
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-29 13:21:03 +0000

    -m

---
+++ 6)collections/Assignment4.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Assignment4 {
+    public static void main(String[] args) {
+        List<Number> numberList = new ArrayList<>();
+        numberList.add(1);
+        numberList.add(2.0);
+        numberList.add(3.0f);
+        System.out.println(numberList);
+    }
+}
