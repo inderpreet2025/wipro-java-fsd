b50a51f105a0d7e64c4720bd09a6051784e35190
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-29 13:31:24 +0000

    -m

---
+++ 6)collections/Assignment1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+
+        ArrayList<String> months = new ArrayList<>();
+
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("August");
+        months.add("September");
+        months.add("October");
+        months.add("November");
+        months.add("December");
+
+        System.out.println("Months of the year:");
+        for (String month : months) {
+            System.out.println(month);
+        }
+    }
+}
