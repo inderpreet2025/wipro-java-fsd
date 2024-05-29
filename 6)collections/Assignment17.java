dd6694a1dd7e70bdcc8de59f465f1464552e0960
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-29 17:28:16 +0000

    -m

---
+++ 6)collections/Assignment17.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Assignment17 {
+
+    private Hashtable<String, String> M1;
+
+    public Assignment17() {
+        M1 = new Hashtable<>();
+    }
+
+    // Method to save country and capital
+    public Hashtable<String, String> saveCountryCapital(String countryName, String capital) {
+        M1.put(countryName, capital);
+        return M1;
+    }
+
+    // Method to get capital by country name
+    public String getCapital(String countryName) {
+        return M1.get(countryName);
+    }
+
+    // Method to get country by capital name
+    public String getCountry(String capitalName) {
+        for (Map.Entry<String, String> entry : M1.entrySet()) {
+            if (entry.getValue().equals(capitalName)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    // Method to create a map with capital as key and country as value
+    public Hashtable<String, String> createCapitalCountryMap() {
+        Hashtable<String, String> M2 = new Hashtable<>();
+        for (Map.Entry<String, String> entry : M1.entrySet()) {
+            M2.put(entry.getValue(), entry.getKey());
+        }
+        return M2;
+    }
+
+    // Method to create a list of all country names
+    public ArrayList<String> getAllCountries() {
+        return new ArrayList<>(M1.keySet());
+    }
+
+    public static void main(String[] args) {
+        Assignment17 countryCapitalMap = new Assignment17();
+
+        // Adding countries and capitals
+        countryCapitalMap.saveCountryCapital("India", "Delhi");
+        countryCapitalMap.saveCountryCapital("Japan", "Tokyo");
+
+        // Get and print capital of a country
+        System.out.println("Capital of India: " + countryCapitalMap.getCapital("India"));
+
+        // Get and print country by capital name
+        System.out.println("Country with capital Tokyo: " + countryCapitalMap.getCountry("Tokyo"));
+
+        // Create and print map with capital as key and country as value
+        Hashtable<String, String> capitalCountryMap = countryCapitalMap.createCapitalCountryMap();
+        System.out.println("Capital-Country Map: " + capitalCountryMap);
+
+        // Get and print list of all countries
+        ArrayList<String> allCountries = countryCapitalMap.getAllCountries();
+        System.out.println("All Countries: " + allCountries);
+    }
+}
