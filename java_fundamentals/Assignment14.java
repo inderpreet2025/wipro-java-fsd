440a087805b04a1273a2cde0f7da3991621f1608
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-29 11:10:36 +0000

    -m

---
+++ java_fundamentals/Assignment14.java
@@ -0,0 +1,11 @@
+public class Assignment14 {
+    public static void main(String[] args) {
+        System.out.println("Even numbers between 23 and 57 are:");
+        
+        for (int i = 23; i <= 57; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
