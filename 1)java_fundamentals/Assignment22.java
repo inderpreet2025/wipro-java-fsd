37dbbb38e9fc60266489af072c1a21f5cf16c772
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-30 06:22:09 +0000

    -m

---
+++ 1)java_fundamentals/Assignment22.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Assignment22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+        int min = array[0];
+
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        scanner.close();
+
+        System.out.println("Maximum value in the array: " + max);
+        System.out.println("Minimum value in the array: " + min);
+    }
+}
