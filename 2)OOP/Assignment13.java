176d06960724ced6360aec26bda33b24a8cbb567
Inderpreet Singh <97393188+inderpreet2025@users.noreply.github.com>
2024-05-30 05:31:17 +0000

    -m

---
+++ 2)OOP/Assignment13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Assignment13 {
+    public static String combineStrings(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return a + b + a;
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(combineStrings("hi", "hello")); 
+        System.out.println(combineStrings("", "hello")); 
+        System.out.println(combineStrings("hi", "")); 
+    }
+}
